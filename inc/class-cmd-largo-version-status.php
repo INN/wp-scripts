4523f982b41b589b6226711be1c84e75929ba8ed
Ryan Nagle <rmnagle@gmail.com>
2015-07-16 11:01:14 -0500

    change the way results for largo version status are formatted

--- inc/class-cmd-largo-version-status.php
+++ inc/class-cmd-largo-version-status.php
@@ -16,14 +16,32 @@ class LargoVersionStatus extends WPScriptCmd {
 		else
 			$site_blog_ids = $wpdb->get_results("SELECT blog_id FROM wp_blogs where blog_id > 1");
 
+		$data = array();
 		$ret = '';
 		foreach ($site_blog_ids as $b) {
 			switch_to_blog($b->blog_id);
 			$stored_largo_version = of_get_option('largo_version');
-			$ret .= get_bloginfo('name') . "\n";
-			$ret .= "    Largo version: " . $stored_largo_version . "\n";
-			$ret .= "\n";
+			if (!empty($stored_largo_version))
+				$data[$stored_largo_version][] = get_bloginfo('name');
+			else
+				$data['No version specified'][] = get_bloginfo('name');
+
 			restore_current_blog();
+		}
+
+		uksort($data, function($a, $b) {
+			if ($a == 'No version specified')
+				return -1;
+			if ($b == 'No version specified')
+				return 1;
+			return version_compare($a, $b);
+		});
+
+		foreach ($data as $label => $sites) {
+			$ret .= 'Largo version: ' . $label . "\n";
+			foreach ($sites as $site)
+				$ret .= "    " . $site . "\n";
+			$ret .= "\n";
 		}
 
 		return $ret;
